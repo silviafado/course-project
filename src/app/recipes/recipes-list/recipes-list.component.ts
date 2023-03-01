15dd5b2d2b2874717fb0e6c499c7c98f1398dfeb
silviafado <silviafado@gmail.com>
2023-03-01 14:26:57 +0100

    Creating components

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', '../../../assets/test-recipe.jpeg')
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
